dbb5d1b4e74bce1f92a4477ac97a1398012f21a5
ChinaBruce <mayanwei@foxmail.com>
2016-04-26 18:21:39 +0800

    测试如何获取版本

--- www/js/app.js
+++ www/js/app.js
@@ -3,54 +3,54 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','$cordovaAppVersion'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider.state('app',{
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'appCtrl'
-  })
-  .state('login',{
-      url:'/login',
-      templateUrl:'templates/login.html',
-      controller:'loginCtrl'
-  })
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
       }
-    }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('app.record', {
-      url: '/record',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/record.html'
-        }
-      }
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'appCtrl'
     })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      })
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
+        }
+      })
+
+      .state('app.record', {
+        url: '/record',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/record.html'
+          }
+        }
+      })
     $urlRouterProvider.otherwise('/login');
-});
+  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,23 +1,24 @@
-angular.module('starter.controllers',[])
-.controller("appCtrl", function($scope, $state) {
-    $scope.logout = function() {
-        $state.go('login');
-    }
-})
-.controller("loginCtrl",function($scope, $http, $state) {
-    $scope.login = function() {
-        var data = 'name=' + $scope.user.name + '&password='+ $scope.user.password +'&deviceType=1&deviceUniqueId=123testsdsdfsdf';
-        var timestamp = (new Date()).valueOf();
-        var signature = md5('Login' + timestamp + 'prestoremoney.common.2016');
-        $http({
-            method:'GET',
-            url:'http://192.168.51.52:88/api/Account/Login?' + data,
-            headers:{'apptype':'2','timestamp':timestamp,'signature':signature}
-        }).success(function (data) {
-            console.log(data);
-        }).error(function(data){
-            console.log(data);
-        });
-        $state.go('app.record');
-    }
-});
+angular.module('starter.controllers', [])
+    .controller("appCtrl", function ($scope, $state) {
+        $scope.logout = function () {
+            $state.go('login');
+        }
+    })
+    .controller("loginCtrl", function ($scope, $http, $state,$cordovaAppVersion) {
+        $scope.user.version = $cordovaAppVersion.getVersionNumber() //获取版本号
+        $scope.login = function () {
+            // var data = 'name=' + $scope.user.name + '&password=' + $scope.user.password + '&deviceType=1&deviceUniqueId=123testsdsdfsdf';
+            // var timestamp = (new Date()).valueOf();
+            // var signature = md5('Login' + timestamp + 'prestoremoney.common.2016');
+            // $http({
+            //     method: 'GET',
+            //     url: 'http://192.168.51.52:88/api/Account/Login?' + data,
+            //     headers: { 'apptype': '2', 'timestamp': timestamp, 'signature': signature }
+            // }).success(function (data) {
+            //     console.log(data);
+            // }).error(function (data) {
+            //     console.log(data);
+            // });
+            $state.go('app.record');
+        }
+    });
