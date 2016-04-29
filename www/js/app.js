b4a8127c8d4170204222bf7b3d5b0e1899b1dd3b
ChinaBruce <mayanwei@foxmail.com>
2016-04-29 18:14:39 +0800

    登录和登出

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var remoteUrl = "http://192.168.51.52:88/api/";
+var remoteUrl = "http://192.168.57.180:45429/api/";//"http://121.28.95.78:93/api/";
 var signatureKey = "prestoremoney.common.2016";
-angular.module('starter', ['ionic', 'starter.controllers','ngCordova'])
-
-  .run(function ($ionicPlatform, $cordovaAppVersion) {
-    $ionicPlatform.ready(function () {
+var token = "";
+angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
+  .run(['$ionicPlatform','$rootScope','$cordovaAppVersion',function ($ionicPlatform, $rootScope, $cordovaAppVersion) {
+    $ionicPlatform.ready(function ($rootScope) {
       if (window.cordova && window.cordova.plugins.Keyboard) {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -22,9 +22,14 @@ angular.module('starter', ['ionic', 'starter.controllers','ngCordova'])
       if (window.StatusBar) {
         StatusBar.styleDefault();
       }
-            
+      
+      function GetAppVersion() {
+        $cordovaAppVersion.getVersionNumber().then(function (data) {
+          $rootScope.version = data;
+        });
+      }
     });
-  })
+  }])
 
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider.state('app', {
@@ -57,3 +62,4 @@ angular.module('starter', ['ionic', 'starter.controllers','ngCordova'])
       })
     $urlRouterProvider.otherwise('/login');
   });
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,55 @@
 angular.module('starter.controllers', [])
-    .controller("appCtrl", function ($scope, $state) {
+    .controller("appCtrl", function ($scope, $state, $http) {
         $scope.logout = function () {
-            $state.go('login');
+            var timestamp = (new Date()).valueOf();
+            var signature = md5('Logout' + timestamp + signatureKey);
+            $http({
+                method: 'GET',
+                url: remoteUrl + 'Account/Logout',
+                headers: { 'apptype': '2', 'timestamp': timestamp, 'signature': signature, 'token': token }
+            }).success(function (response) {
+                if (response.IsSuccess) {
+                    $state.go('login');
+                }
+                else
+                {
+                    alert(response.Message);
+                }
+            }).error(function (response) {
+                console.log(response);
+            });
         }
     })
-    .controller("loginCtrl", function ($scope, $http, $state, $cordovaAppVersion) {
-        $cordovaAppVersion.getVersionNumber().then(function (version) {
-            $scope.version = version;
-        });
-        $scope.login = function () {
-            // var data = 'name=' + $scope.user.name + '&password=' + $scope.user.password + '&deviceType=1&deviceUniqueId=123testsdsdfsdf';
-            // var timestamp = (new Date()).valueOf();
-            // var signature = md5('Login' + timestamp + 'prestoremoney.common.2016');
-            // $http({
-            //     method: 'GET',
-            //     url: 'http://192.168.51.52:88/api/Account/Login?' + data,
-            //     headers: { 'apptype': '2', 'timestamp': timestamp, 'signature': signature }
-            // }).success(function (data) {
-            //     console.log(data);
-            // }).error(function (data) {
-            //     console.log(data);
-            // });
+    .controller("loginCtrl", function ($scope, $http, $state, $ionicModal) {
 
-            $state.go('app.record');
-        }
+        $scope.user = {};
+
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        $scope.doLogin = function () {
+            var data = 'name=' + $scope.user.username + '&password=' + md5($scope.user.password) + '&deviceUniqueId=123testsdsdfsdf';
+            var timestamp = (new Date()).valueOf();
+            var signature = md5('Login' + timestamp + signatureKey);
+            $http({
+                method: 'GET',
+                url: remoteUrl + 'Account/Login?' + data,
+                headers: { 'apptype': '2', 'timestamp': timestamp, 'signature': signature }
+            }).success(function (response) {
+                if (response.IsSuccess) {
+                    token = response.Data.Token;
+                    $state.go('app.record');
+                }
+                else
+                {
+                    alert(response.Message);
+                }
+            }).error(function (response) {
+                console.log(response);
+            });
+        };
     });
